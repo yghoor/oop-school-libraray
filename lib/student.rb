226da7cbd2dc6654e8afd643ea21073b0bb67e72
Yahya Ghoor <ghooryahya@gmail.com>
2022-10-04 21:35:49 +0200

    Add readable person type instance variables

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,10 @@
 class Student < Person
-  attr_reader :classroom
+  attr_reader :classroom, :person_type
 
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
+    @person_type = 'student'
   end
 
   def play_hooky
--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,7 +1,10 @@
 class Teacher < Person
+  attr_reader :person_type
+
   def initialize(specialization, age, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @specialization = specialization
+    @person_type = 'teacher'
   end
 
   def can_use_services?
