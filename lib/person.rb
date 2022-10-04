76a1866687b09cac6e69ee2052bb6004a3054ac7
Yahya Ghoor <ghooryahya@gmail.com>
2022-10-04 13:07:10 +0200

    Implement many to many with 'Book' using 'Rentals'

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,7 @@ require './corrector'
 
 class Person
   attr_reader :id
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
@@ -10,6 +10,7 @@ class Person
     @name = name
     @parent_permission = parent_permission
     @corrector = Corrector.new
+    @rentals = []
   end
 
   def can_use_services?
@@ -18,6 +19,10 @@ class Person
 
   def validate_name
     @name = @corrector.correct_name(@name)
+  end
+
+  def add_rental(date, book)
+    Rental.new(date, self, book)
   end
 
   private
