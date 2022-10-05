6e771eb53c384549798b5a70f2a59eb6524f8864
Yahya Ghoor <ghooryahya@gmail.com>
2022-10-05 17:58:52 +0200

    Build method to create people

--- lib/main.rb
+++ lib/main.rb
@@ -32,4 +32,53 @@ Please choose an option by entering a number:
       puts "#{i + 1}) [#{person.class}] Name: #{person.name}, ID: #{person.id} Age: #{person.age}"
     end
   end
+
+  def ui_create_person
+    print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
+
+    person_option = gets.chomp
+
+    loop do
+      break if person_option.to_i == 1 || person_option.to_i == 2
+
+      print 'Please enter a valid number (1 or 2): '
+      person_option = gets.chomp
+    end
+
+    print 'Age: '
+    age = gets.chomp.to_i
+
+    print 'Name: '
+    name = gets.chomp
+
+    case person_option
+    when '1'
+      print 'Has parent permission? [Y/N]: '
+      permission = gets.chomp
+
+      loop do
+        if %w[Y y].include?(permission)
+          permission = true
+          break
+        elsif %w[N n].include?(permission)
+          permission = false
+          break
+        else
+          puts 'Please enter a valid character [Y/N]: '
+          permission = gets.chomp
+        end
+      end
+
+      @people.push(Student.new(nil, age, name, permission))
+      @people[-1].validate_name
+      puts 'Person created successfully'
+    when '2'
+      print 'Specialization: '
+      specialization = gets.chomp
+
+      @people.push(Teacher.new(specialization, age, name))
+      @people[-1].validate_name
+      puts 'Person created successfully'
+    end
+  end
 end
